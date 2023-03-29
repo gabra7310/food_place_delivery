7cd315aa2d48ce9971545bfbcccb586f1e2afe2c
Gabriel Alves <66787478+gabra7310@users.noreply.github.com>
2023-03-29 17:43:15 -0300

    Update 1_🏢_Visão_Empresa.py

--- pages/1_🏢_Visão_Empresa.py
+++ pages/1_🏢_Visão_Empresa.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 from streamlit_folium import folium_static
 
-st.set_page_config(page_title = "Visão Empresa", page_icon='🏢', , layout='wide')
+st.set_page_config(page_title = "Visão Empresa", page_icon='🏢', layout='wide')
 
 # Import dataset
 df_raw = pd.read_csv('train.csv')
